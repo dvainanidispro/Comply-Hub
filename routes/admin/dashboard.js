9c0d43ad3fdf18f65fa5ef40be999e9eea2834f6
Dimitris Vainanidis <vainanidis@computerstudio.gr>
2026-06-03 10:36:33 +0300

    0.17.3 - Admin dashboard basic

--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -1,14 +1,25 @@
 import { Router } from 'express';
+import Cache from '../../models/cache.js';
+import log from '../../lib/logger.js';
 
 const dashboard = Router();
 
 // Αρχική σελίδα (dashboard)
 dashboard.get(['/', '/dashboard'], 
-    (req, res) => {
-    res.render('admin/dashboard', {
-        layout: 'main',
-        user: req.user,
-    });
+    async (req, res) => {
+
+        const users = await Cache.table.User;
+        const usersByRole = Object.groupBy(users, user=>user.role);
+
+        const organizations = await Cache.table.Organization;
+
+        res.render('admin/dashboard', {
+            layout: 'main',
+            user: req.user,
+            organizations,
+            users,
+            usersByRole
+        });
 });
 
 
